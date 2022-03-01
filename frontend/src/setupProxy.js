7c26060167d89f7a27bd768276e5e9bf5d7a4ce2
blckassassin <jmartinetti@mailfence.com>
2022-03-01 10:22:34 -0800

    updated proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = app => {
   app.use(
     "/api",
     proxy({
-      target: process.env.REACT_APP_API_HOST || "http://localhost:8000",
+      target: "https://plaidbacknode.eastus.azurecontainer.io:8000",
       changeOrigin: true
     })
   );
